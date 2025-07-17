59a2736d8d7390b5d963aaaf4c07ceb70907e847
Miguel Granda Martínez <mgranda@comunitea.com>
2025-07-17 13:36:42 +0200

    [FIX] comprobar si la factura tiene diario establecido

--- limp_reports/models/account_invoice.py
+++ limp_reports/models/account_invoice.py
@@ -8,7 +8,7 @@ class AccountInvoice(models.Model):
     _inherit = "account.invoice"
 
     def action_invoice_open(self):
-        if 'Scont' not in self.journal_id.name:
+        if self.journal_id and 'Scont' not in self.journal_id.name:
             for line in self.invoice_line_ids:
                 if not line.invoice_line_tax_ids:
                     raise UserError(_("You must select a tax for each invoice line."))
